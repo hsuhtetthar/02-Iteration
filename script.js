6c4fa460030328ce20054d7f20f98743da011161
hsuhtetthar <hhthar.028278@gmail.com>
2024-04-28 14:39:46 +0630

    first commit for iteration

---
+++ script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+let students = ["Hsu", "Htet", "Thar"];
+
+console.log(students.length);
+
+for (let i = 0; i < students.length; i++) {
+  console.log("Nice to meet you, ", students[i], "!");
+}
